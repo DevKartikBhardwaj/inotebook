eb0edb374cd41c905ee3fe1fa169255fa79927a0
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2022-10-22 16:08:21 +0530

    completion of vid56

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './components/Home'
+import About from './components/About'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
-      <h2>Hello my name is kartik bhidu</h2>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
